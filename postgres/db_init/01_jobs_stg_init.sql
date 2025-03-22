86beb84c816a103e526691cec83a4a9eeba7f0a0
Daniel <thefunnyonion@gmail.com>
2025-03-22 11:17:05 +0900

    added postgres admin

--- postgres/db_init/01_jobs_stg_init.sql
+++ postgres/db_init/01_jobs_stg_init.sql
@@ -1,12 +1,16 @@
 -- Create table_A if it does not exist
 
-CREATE TABLE IF NOT EXISTS table_A (
+CREATE TABLE IF NOT EXISTS joblistings (
     job_id SERIAL PRIMARY KEY,
     website TEXT,
-    job_url	TEXT,
+    job_url TEXT NOT NULL,
     job_url_direct TEXT,
-    title TEXT,
-    company TEXT,
+    title TEXT NOT NULL,
+    company TEXT NOT NULL,
     date_posted DATE,
-    
+    job_description TEXT NOT NULL,
+    company_industry TEXT,
+    company_url TEXT,
+    company_num_employees TEXT,
+    company_description TEXT
 );
